c11da82c4aa2b3fd1be57c3670f8e0311a82e343
adryano.chavier <adryano.chavier@senac.br>
2025-08-15 15:38:19 -0300

    Implemented Interfaces and classes for Domain.Core

---
+++ MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MessegaType { get; protected set; }
+        protected Message()
+        {
+            MessegaType = GetType().Name;
+        }   
+    }
+}
